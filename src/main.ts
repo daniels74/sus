1def63b023e7137e2a24f983c21962189dc4cb66
ctrlzdan <danielsfinance4774@gmail.com>
2025-08-19 18:32:32 -0500

    some sus updates

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/Root/app.config';
 import { AppComponent } from './app/Root/app.component';
+import { inject, track } from '@vercel/analytics';
 
-bootstrapApplication(AppComponent, appConfig)
+inject();
+
+bootstrapApplication(AppComponent, appConfig).then(() => {
+  track('app_boot');
+})
   .catch((err) => console.error(err));
